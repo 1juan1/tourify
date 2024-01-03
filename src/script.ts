2aa0a8c834366078ed225c1a29a8fbfce5b1ff74
juanyep <yepezjuan6@gmail.com>
2024-01-03 11:13:57 -0800

    added the top 10 artist images

--- src/script.ts
+++ src/script.ts
@@ -1,4 +1,4 @@
-const clientId = ""; // Replace with your client id
+const clientId = "ecc7a12366d74791bc02378af6adc309"; // Replace with your client id
 const params = new URLSearchParams(window.location.search)
 const code = params.get("code");
 
@@ -92,21 +92,64 @@ async function fetchTopTen(token: string): Promise<any> {
 function populateUI(profile: any) {
     document.getElementById("displayName")!.innerText = profile.display_name;
     if (profile.images[0]) {
-        const profileImage = new Image(200, 200);
+        const profileImage = new Image(100, 100);
         profileImage.src = profile.images[0].url;
         document.getElementById("avatar")!.appendChild(profileImage);
     }
     document.getElementById("id")!.innerText = profile.id;
-    document.getElementById("email")!.innerText = profile.email;
-    document.getElementById("uri")!.innerText = profile.uri;
-    document.getElementById("uri")!.setAttribute("href", profile.external_urls.spotify);
     document.getElementById("url")!.innerText = profile.href;
     document.getElementById("url")!.setAttribute("href", profile.href);
     document.getElementById("imgUrl")!.innerText = profile.images[0]?.url ?? '(no profile image)';
 }
 
 function populateArtists(top_artists: any) {
-    document.getElementById("topartist")!.innerText = top_artists.items[0].name;
+
+    document.getElementById("top0")!.innerText = top_artists.items[0].name;
+    document.getElementById("top1")!.innerText = top_artists.items[1].name;
+    document.getElementById("top2")!.innerText = top_artists.items[2].name;
+    document.getElementById("top3")!.innerText = top_artists.items[3].name;
+    document.getElementById("top4")!.innerText = top_artists.items[4].name;
+    document.getElementById("top5")!.innerText = top_artists.items[5].name;
+    document.getElementById("top6")!.innerText = top_artists.items[6].name;
+    document.getElementById("top7")!.innerText = top_artists.items[7].name;
+    document.getElementById("top8")!.innerText = top_artists.items[8].name;
+    document.getElementById("top9")!.innerText = top_artists.items[9].name;
+
+    const image_artist0 = new Image(200, 200);
+    const image_artist1 = new Image(200, 200);
+    const image_artist2 = new Image(200, 200);
+    const image_artist3 = new Image(200, 200);
+    const image_artist4 = new Image(200, 200);
+    const image_artist5 = new Image(200, 200);
+    const image_artist6 = new Image(200, 200);
+    const image_artist7 = new Image(200, 200);
+    const image_artist8 = new Image(200, 200);
+    const image_artist9 = new Image(200, 200);
+
+    image_artist0.src = top_artists.items[0].images[0].url;
+    image_artist1.src = top_artists.items[1].images[0].url;
+    image_artist2.src = top_artists.items[2].images[0].url;
+    image_artist3.src = top_artists.items[3].images[0].url;
+    image_artist4.src = top_artists.items[4].images[0].url;
+    image_artist5.src = top_artists.items[5].images[0].url;
+    image_artist6.src = top_artists.items[6].images[0].url;
+    image_artist7.src = top_artists.items[7].images[0].url;
+    image_artist8.src = top_artists.items[8].images[0].url;
+    image_artist9.src = top_artists.items[9].images[0].url;
+
+
+    document.getElementById("img0")!.appendChild(image_artist0);
+    document.getElementById("img1")!.appendChild(image_artist1);
+    document.getElementById("img2")!.appendChild(image_artist2);
+    document.getElementById("img3")!.appendChild(image_artist3);
+    document.getElementById("img4")!.appendChild(image_artist4);
+    document.getElementById("img5")!.appendChild(image_artist5);
+    document.getElementById("img6")!.appendChild(image_artist6);
+    document.getElementById("img7")!.appendChild(image_artist7);
+    document.getElementById("img8")!.appendChild(image_artist8);
+    document.getElementById("img9")!.appendChild(image_artist9);
+
+
 }
 
 
