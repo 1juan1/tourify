ea6b4a8f86cda4f8e2f9388d1c6cd29eb2751f6b
juanyep <yepezjuan6@gmail.com>
2024-11-15 09:48:00 -0800

    used Javascript DOM manipulation to shorten amount of code

--- src/script.ts
+++ src/script.ts
@@ -11,7 +11,6 @@ if (!code) {
     const top_artists = await fetchTopTen(accessToken);
     populateArtists(top_artists);
 
-
 }
 
 export async function redirectToAuthCodeFlow(clientId: string) {
@@ -24,7 +23,7 @@ export async function redirectToAuthCodeFlow(clientId: string) {
     params.append("client_id", clientId);
     params.append("response_type", "code");
     params.append("redirect_uri", "http://localhost:5173/callback");
-    params.append("scope", "user-read-private user-read-email user-top-read");
+    params.append("scope", "user-read-private user-read-email user-top-read"); // this is where I fixed my code
     params.append("code_challenge_method", "S256");
     params.append("code_challenge", challenge);
 
@@ -76,6 +75,8 @@ async function fetchProfile(token: string): Promise<any> {
         method: "GET", headers: { Authorization: `Bearer ${token}` }
     });
 
+    // it would be a good idea to add error handling & network issues 
+
     return await result.json();
 }
 
@@ -104,50 +105,23 @@ function populateUI(profile: any) {
 
 function populateArtists(top_artists: any) {
 
-    document.getElementById("top0")!.innerText = top_artists.items[0].name;
-    document.getElementById("top1")!.innerText = top_artists.items[1].name;
-    document.getElementById("top2")!.innerText = top_artists.items[2].name;
-    document.getElementById("top3")!.innerText = top_artists.items[3].name;
-    document.getElementById("top4")!.innerText = top_artists.items[4].name;
-    document.getElementById("top5")!.innerText = top_artists.items[5].name;
-    document.getElementById("top6")!.innerText = top_artists.items[6].name;
-    document.getElementById("top7")!.innerText = top_artists.items[7].name;
-    document.getElementById("top8")!.innerText = top_artists.items[8].name;
-    document.getElementById("top9")!.innerText = top_artists.items[9].name;
 
-    const image_artist0 = new Image(200, 200);
-    const image_artist1 = new Image(200, 200);
-    const image_artist2 = new Image(200, 200);
-    const image_artist3 = new Image(200, 200);
-    const image_artist4 = new Image(200, 200);
-    const image_artist5 = new Image(200, 200);
-    const image_artist6 = new Image(200, 200);
-    const image_artist7 = new Image(200, 200);
-    const image_artist8 = new Image(200, 200);
-    const image_artist9 = new Image(200, 200);
+    const artistList = document.getElementById("artist-list");
+    artistList!.innerHTML = ` `;
 
-    image_artist0.src = top_artists.items[0].images[0].url;
-    image_artist1.src = top_artists.items[1].images[0].url;
-    image_artist2.src = top_artists.items[2].images[0].url;
-    image_artist3.src = top_artists.items[3].images[0].url;
-    image_artist4.src = top_artists.items[4].images[0].url;
-    image_artist5.src = top_artists.items[5].images[0].url;
-    image_artist6.src = top_artists.items[6].images[0].url;
-    image_artist7.src = top_artists.items[7].images[0].url;
-    image_artist8.src = top_artists.items[8].images[0].url;
-    image_artist9.src = top_artists.items[9].images[0].url;
+    top_artists.items.forEach((artist: any) => {
+        const artistItem = document.createElement('li');
 
+        artistItem.innerHTML = `
+        <div class="a-list">
+          <h2 > ${artist.name}</h2>
+          <img src = "${artist.images[0].url}" alt= "artist image">
+    
+        </div>
+        `
+        artistList!.appendChild(artistItem);
+    });
 
-    document.getElementById("img0")!.appendChild(image_artist0);
-    document.getElementById("img1")!.appendChild(image_artist1);
-    document.getElementById("img2")!.appendChild(image_artist2);
-    document.getElementById("img3")!.appendChild(image_artist3);
-    document.getElementById("img4")!.appendChild(image_artist4);
-    document.getElementById("img5")!.appendChild(image_artist5);
-    document.getElementById("img6")!.appendChild(image_artist6);
-    document.getElementById("img7")!.appendChild(image_artist7);
-    document.getElementById("img8")!.appendChild(image_artist8);
-    document.getElementById("img9")!.appendChild(image_artist9);
 
 
 }
